37181c7088ac445fa0378f5d0aa15f70a5b35113
maton369 <ysk901085@gmail.com>
2024-12-30 01:17:49 +0000

    [Add] Update

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE>
+<!DOCTYPE html>
+<html lang="{{str_replace('_','-',app()->getLocale())}}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+<body>
+    <h1 class="title">編集画面</h1>
+    <div class="content">
+        <form action="/posts/{{ $post->id }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class='content__title'>
+                <h2>タイトル</h2>
+                <input type='text' name='post[title]' value="{{ $post->title }}">
+            </div>
+            <div class='content__body'>
+                <h2>本文</h2>
+                <input type='text' name='post[body]' value="{{ $post->body }}">
+            </div>
+            <input type="submit" value="保存">
+        </form>
+    </div>
+</body>
+</html>
